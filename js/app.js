0739cb83e237253e2832e881cfe4293268da3d67
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-06-25 17:10:19 -0400

    Update

---
+++ js/app.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let navbar = document.querySelector(".navbar");
+let hamburgerBtn = document.querySelector(".navbar__hamburger-btn");
+let mobileNav = document.querySelector(".navbar__nav");
+
+hamburgerBtn.addEventListener("click", () => {
+  if (!mobileNav.classList.contains("navbar__nav--active")) {
+    mobileNav.classList.add("navbar__nav--active");
+    hamburgerBtn.src = "./images/icon-close.svg";
+    navbar.classList.add("navbar--overlay-menu");
+  } else {
+    mobileNav.classList.remove("navbar__nav--active");
+    hamburgerBtn.src = "./images/icon-hamburger.svg";
+    navbar.classList.remove("navbar--overlay-menu");
+  }
+});
